25bd9be3e85581d6bdc526109cca02a9b665f741
samjam <6parsam@gmail.com>
2020-02-25 19:29:32 -0500

    falling platforms

---
+++ Balltower/Assets/fallingcam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingcam : MonoBehaviour
+{
+    public bool falling = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(falling)
+            GameObject.Find("Main Camera").GetComponent<smoothefollow>().movementTime = 0.001f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name== "Floor")
+        {
+            GameObject.Find("Main Camera").GetComponent<smoothefollow>().movementTime = 0.24f;
+            falling = false;
+        }
+    }
+}
---
+++ Balltower/Assets/platformtrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformtrigger : MonoBehaviour
+{
+    public GameObject targetplatform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ThirdPersonController_LITE")
+        {
+            // platform fall.
+            targetplatform
+                .GetComponent<fallingscript>()
+                .falling = true;
+
+        }
+    }
+}
