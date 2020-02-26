908617b6d525ff61dd4c6bb30af1007a2f5d3702
M33-Melissa <melissa_lim_17@hotmail.com>
2020-02-25 19:39:41 -0500

    Added Ball Spawning

---
+++ Balltower/Assets/Scripts/Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public BallSpawner spawner = null;
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //spawn balls
+            Renderer renderer = GetComponent<Renderer>();
+            spawner.SpawnBalls(renderer.material.color);
+        }
+    }
+}
