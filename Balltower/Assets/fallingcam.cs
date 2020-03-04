34b3028f8b15ef93d0443f1ead16893a25c0e352
samjam <6parsam@gmail.com>
2020-03-03 21:34:50 -0500

    click to kick done

--- Balltower/Assets/fallingcam.cs
+++ Balltower/Assets/fallingcam.cs
@@ -7,20 +7,41 @@ public class fallingcam : MonoBehaviour
     public bool falling = false;
     public GameObject arrow;
     public GameObject trigger;
+    float kickCoolDown = 0;
+    public float kickCoolDownDuration = 1f;
+    public GameObject projectile;
+
 
     void Update()
     {
+        // speed up camera follow during fall
         if(falling)
             GameObject.Find("Main Camera").GetComponent<smoothefollow>().movementTime = 0.001f;
-
+        // move arrow
         arrow.transform.LookAt(trigger.transform, -arrow.transform.up);
         arrow.transform.Rotate(new Vector3(-90, 0, 0));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Time.time > kickCoolDown)
+            {
+                
+                float distance = 2f;
+                Vector3 spawnpos = transform.forward * distance + transform.position;
+
+                GameObject newBall = Instantiate(projectile, spawnpos, transform.rotation);
+                Rigidbody rigidbody = newBall.GetComponent<Rigidbody>();
+                rigidbody.velocity = transform.forward * 10f;
+                kickCoolDown = Time.time + kickCoolDownDuration;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Floor")
         {
+            // reset camera upon hitting the ground
             GameObject.Find("Main Camera").GetComponent<smoothefollow>().movementTime = 0.24f;
             falling = false;
         }
