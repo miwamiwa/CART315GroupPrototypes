bd38bb46b747e3a94e428b638788c96b76620a0c
samjam <6parsam@gmail.com>
2020-03-07 13:54:53 -0500

    camera follow proto

---
+++ Tilting Dog/Assets/camerafollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("ThirdPersonController_LITE");
+        Vector3 playerPos = player.GetComponent<Transform>().position ;
+        Vector3 playerVel = player.GetComponent<Rigidbody>().velocity;
+        if (playerVel.z > 0) playerVel.z = 0;
+        transform.position = playerPos - new Vector3(0, -2f, 2f - playerVel.z) ;
+    }
+}
