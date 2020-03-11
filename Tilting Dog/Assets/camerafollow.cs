2ee1cca36be64ad4f1e5dea600c9209d6330fdb8
samjam <6parsam@gmail.com>
2020-03-10 23:15:00 -0400

    added cam switching

--- Tilting Dog/Assets/camerafollow.cs
+++ Tilting Dog/Assets/camerafollow.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class camerafollow : MonoBehaviour
 {
-    int doggyDir = 1;
+    int doggyDir = -1;
     int lastDoggyDir = 1;
+    int tick = 0;
+    GameObject dog;
+    public bool panOut = false;
+    bool prevPanOut = false;
+    float displacement = 0f;
+    Vector3 initialPosition;
+    public bool pause = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dog = GameObject.Find("DOGGOANIM");
     }
 
     // Update is called once per frame
     void Update()
     {
-        doggyDir = -1;
-        if(doggyDir != lastDoggyDir)
+        
+        if (doggyDir != lastDoggyDir)
         {
             transform.eulerAngles = new Vector3(
               transform.eulerAngles.x,
@@ -28,14 +36,64 @@ public class camerafollow : MonoBehaviour
         lastDoggyDir = doggyDir;
 
 
+        if (panOut && !prevPanOut)
+        {
+            displacement = 0f;
+            tick = 0;
+            pause = true;
+            transform.eulerAngles = new Vector3(
+              transform.eulerAngles.x,
+              transform.eulerAngles.y - 90,
+              transform.eulerAngles.z
+              );
 
-        GameObject player = GameObject.Find("ThirdPersonController_LITE");
-        Vector3 playerPos = player.GetComponent<Transform>().position ;
-        Vector3 playerVel = player.GetComponent<Rigidbody>().velocity;
-        if (playerVel.z > 0 && doggyDir == 1) playerVel.z = 0;
-        if (playerVel.z < 0 && doggyDir == -1) playerVel.z = 0;
+            initialPosition = transform.position;
+        }
 
-        transform.position = playerPos - new Vector3(0, -2f, doggyDir* (2f )- playerVel.z );
-        
+        prevPanOut = panOut;
+
+        if (panOut)
+        {
+            
+            displacement += doggyDir*0.12f;
+            tick++;
+            if(tick >= 380)
+            {
+                if (!pause)
+                {
+                   
+                }
+                else
+                {
+                    GameObject.Find("Button-emergency").GetComponent<platformmotion>().enabled = true;
+                    GameObject.Find("Button-emergency (1)").GetComponent<platformmotion>().enabled = true;
+                }
+                
+
+                
+            }
+            else transform.position = initialPosition + new Vector3(displacement, 0, 0);
+        }
+        else
+        {
+            GameObject player = GameObject.Find("ThirdPersonController_LITE");
+            Vector3 playerPos = player.GetComponent<Transform>().position;
+            Vector3 playerVel = player.GetComponent<Rigidbody>().velocity;
+            if (playerVel.z > 0 && doggyDir == 1) playerVel.z = 0;
+            if (playerVel.z < 0 && doggyDir == -1) playerVel.z = 0;
+
+            transform.position = playerPos - new Vector3(0, -2f, doggyDir * (2f) - playerVel.z);
+        }        
+    }
+
+    public void unpause()
+    {
+        doggyDir *= -1;
+        panOut = false;
+        transform.eulerAngles = new Vector3(
+      transform.eulerAngles.x,
+      transform.eulerAngles.y + 90,
+      transform.eulerAngles.z
+      );
     }
 }
---
+++ Tilting Dog/Assets/platformmotion.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformmotion : MonoBehaviour
+{
+    bool triggered = false;
+    public int dir = 1;
+    int frames = 0;
+    GameObject target;
+    public bool enabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("DOGGOANIM");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered&&enabled)
+        {
+
+            Debug.Log("triggered");
+            target.transform.Rotate(-0.0007f*dir*frames,0,0);
+            frames+=1;
+            if(frames>400)
+            {
+                Debug.Log("done " + target.transform.rotation.x);
+                triggered = false;
+                frames = 0;
+                GameObject.Find("Button-emergency").GetComponent<platformmotion>().enabled = false;
+                GameObject.Find("Button-emergency (1)").GetComponent<platformmotion>().enabled = false;
+                GameObject.Find("camTarget").GetComponent<camerafollow>().unpause();
+                target.GetComponent<doggoController>().dir = dir;
+                if (dir == 1)
+                {
+                    GameObject.Find("Button-emergency").transform.position = new Vector3(0f, -65f, -21f);
+                    GameObject.Find("Button-emergency (1)").transform.position = new Vector3(0f, -51f, -2f);
+                }
+                else
+                {
+                    GameObject.Find("Button-emergency").transform.position = new Vector3(0f, -40f, -13f);
+                    GameObject.Find("Button-emergency (1)").transform.position = new Vector3(0f, -48f, -9f);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "ThirdPersonController_LITE"&&!triggered && target.GetComponent<doggoController>().dir != dir)
+        {
+            Debug.Log("boom "+ target.transform.rotation.x);
+            triggered = true;
+            GameObject.Find("camTarget").GetComponent<camerafollow>().panOut = true;
+        }
+    }
+}
