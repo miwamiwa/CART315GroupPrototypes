4742f9902fb9dab603078c3ab100b4b8f5feb388
M33-Melissa <melissa_lim_17@hotmail.com>
2020-03-11 01:57:17 -0400

    Added Counter UI + Pepperoni Counter

---
+++ Tilting Dog/Assets/PeppeCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PeppeCounter : MonoBehaviour
+{
+    
+    Text pepeCount; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pepeCount = GetComponent<Text>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pepeCount.text = "Pepperoni Counter: " + pepebutton.peppeNum;
+    }
+}
