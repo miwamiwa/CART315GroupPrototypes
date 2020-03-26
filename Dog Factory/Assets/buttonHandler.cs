fe0b1ade4b5bb1728679f918a78a8a4682c9b33b
samjam <6parsam@gmail.com>
2020-03-26 18:10:55 -0400

    t pose and button press

---
+++ Dog Factory/Assets/buttonHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonHandler : MonoBehaviour
+{
+    bool pushed = false;
+    bool lastState = false;
+    float resetTime = 0f;
+    float resetTime2 = 0f;
+    int counter = 0;
+    GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pushed && !lastState) counter = 0;
+        
+
+        if (pushed && lastState)
+        {
+            Vector3 t = transform.Find("Circle").transform.position;
+            
+            if (counter>35)
+            {
+
+                if (counter > 200)
+                {
+                    transform.Find("Circle").transform.position = new Vector3(t.x + 0.009f, t.y, t.z);
+                    if (counter > 235) pushed = false;
+                    
+                   
+                }
+                
+            }
+            else transform.Find("Circle").transform.position = new Vector3(t.x - 0.009f, t.y, t.z);
+
+            counter++;
+        }
+
+        lastState = pushed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pushed = true;
+            Debug.Log("pushin");
+        }
+    }
+}
---
+++ Dog Factory/Assets/playercontrols.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrols : MonoBehaviour
+{
+    public GameObject projectile;
+    float kickCoolDown = 0;
+    float kickCoolDownDuration = 0.5f;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Time.time > kickCoolDown)
+            {
+
+                float distance = 2f;
+                Vector3 spawnpos = transform.forward * distance + transform.position;
+
+                GameObject newBall = Instantiate(projectile, spawnpos, transform.rotation);
+                Rigidbody rigidbody = newBall.GetComponent<Rigidbody>();
+                rigidbody.velocity = transform.forward * 10f;
+                kickCoolDown = Time.time + kickCoolDownDuration;
+
+                anim.Play("t-pose");
+            }
+        }
+    }
+}
