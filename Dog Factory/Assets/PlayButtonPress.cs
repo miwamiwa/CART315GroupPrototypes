65ecd5fd66c61d42bcdcea9286e6fb02b09c713a
M33-Melissa <melissa_lim_17@hotmail.com>
2020-03-31 18:19:55 -0400

    Added button animations, set them up on a new scene in case i messed up your scene

---
+++ Dog Factory/Assets/PlayButtonPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButtonPress : MonoBehaviour
+{
+    [SerializeField] private Animator animationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animationController.SetBool("PlayPress", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animationController.SetBool("PlayPress", false);
+        }
+    }
+}
