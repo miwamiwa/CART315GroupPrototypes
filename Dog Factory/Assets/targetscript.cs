ace4f8759b73059771f61e4858bcd6600b08c9f0
M33-Melissa <melissa_lim_17@hotmail.com>
2020-04-01 01:48:43 -0400

    Fixed Collision script

--- Dog Factory/Assets/targetscript.cs
+++ Dog Factory/Assets/targetscript.cs
@@ -9,6 +9,7 @@ public class targetscript : MonoBehaviour
     public Material mat3;
     public Material mat4;
     public Material mat5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +24,37 @@ public class targetscript : MonoBehaviour
 
     private void OnCollisionStay(Collision collision)
     {
+
+        string matName1 = (mat1.name + " (Instance)");
+        string matName2 = (mat2.name + " (Instance)");
+        string matName3 = (mat3.name + " (Instance)");
+        string matName4 = (mat4.name + " (Instance)");
+        string matName5 = (mat5.name + " (Instance)");
+
         if (collision.gameObject.tag == "Dog")
         {
             Debug.Log("dog");
-            if (collision.gameObject.GetComponent<Renderer>().material==mat1)
+            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName1)
+            {
+                Debug.Log("yo");
+                Destroy(collision.gameObject);
+            }
+            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName2)
+            {
+                Debug.Log("yo");
+                Destroy(collision.gameObject);
+            }
+            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName3)
+            {
+                Debug.Log("yo");
+                Destroy(collision.gameObject);
+            }
+            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName4)
+            {
+                Debug.Log("yo");
+                Destroy(collision.gameObject);
+            }
+            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName5)
             {
                 Debug.Log("yo");
                 Destroy(collision.gameObject);
