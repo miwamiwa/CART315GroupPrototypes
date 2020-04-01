f9665959ecda0a34507caa0e168b1331765424e0
M33-Melissa <melissa_lim_17@hotmail.com>
2020-04-01 02:42:56 -0400

    Added colored targets

--- Dog Factory/Assets/targetscript.cs
+++ Dog Factory/Assets/targetscript.cs
@@ -4,59 +4,17 @@ using UnityEngine;
 
 public class targetscript : MonoBehaviour
 {
-    public Material mat1;
-    public Material mat2;
-    public Material mat3;
-    public Material mat4;
-    public Material mat5;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    public Material DogMaterial;
+    
     private void OnCollisionStay(Collision collision)
     {
 
-        string matName1 = (mat1.name + " (Instance)");
-        string matName2 = (mat2.name + " (Instance)");
-        string matName3 = (mat3.name + " (Instance)");
-        string matName4 = (mat4.name + " (Instance)");
-        string matName5 = (mat5.name + " (Instance)");
+        string matName = (DogMaterial.name + " (Instance)");
 
         if (collision.gameObject.tag == "Dog")
         {
-            Debug.Log("dog");
-            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName1)
+            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName)
             {
-                Debug.Log("yo");
-                Destroy(collision.gameObject);
-            }
-            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName2)
-            {
-                Debug.Log("yo");
-                Destroy(collision.gameObject);
-            }
-            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName3)
-            {
-                Debug.Log("yo");
-                Destroy(collision.gameObject);
-            }
-            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName4)
-            {
-                Debug.Log("yo");
-                Destroy(collision.gameObject);
-            }
-            if (collision.gameObject.GetComponent<MeshRenderer>().material.name == matName5)
-            {
-                Debug.Log("yo");
                 Destroy(collision.gameObject);
             }
         }
