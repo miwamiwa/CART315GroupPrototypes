013689e2f22013f37e67f1cb6c88055ee696d3a2
samjam <6parsam@gmail.com>
2020-04-01 01:48:04 -0400

    life changing peppe updates

---
+++ Dog Factory/Assets/peppescript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class peppescript : MonoBehaviour
+{
+    int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter > 600) Destroy(gameObject);
+    }
+}
