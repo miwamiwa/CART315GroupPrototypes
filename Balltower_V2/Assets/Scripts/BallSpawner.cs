30e715f6743eb7175a5bcf3a80f55ca0bb277eef
M33-Melissa <melissa_lim_17@hotmail.com>
2020-04-07 20:25:37 -0400

    Added Balltower Iteration 1

---
+++ Balltower_V2/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ball = null;
+    public List<Transform> ballTransform = null;
+    public float maxVelocity = 3f;
+    public Material mat = null;
+
+    public void SpawnBalls(Color color)
+    {
+        foreach (Transform transform in ballTransform)
+        {
+
+            GameObject newBall = Instantiate(ball, transform.position, Quaternion.identity);
+            newBall.GetComponent<moreballs>().canSpawn = false;
+            Renderer renderer = newBall.GetComponent<Renderer>();
+            Rigidbody rigidbody = newBall.GetComponent<Rigidbody>();
+            rigidbody.velocity = new Vector3(Random.Range(0f, maxVelocity), Random.Range(0f, maxVelocity), Random.Range(0f, maxVelocity));
+            renderer.material = mat;
+        }
+    }
+}
---
+++ Balltower_V2/Assets/platformtrigger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformtrigger : MonoBehaviour
+{
+    public GameObject targetplatform;
+    public BallSpawner spawner = null;
+    public GameObject arrow;
+
+    float nextTime = 0f;
+    public float interval = 8f;
+    public float displayTime = 3f;
+    bool displayed = false;
+    Quaternion beamrot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrow = GameObject.Find("arrow");
+        arrow.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrow.transform.rotation = beamrot;
+
+        if (Time.time > nextTime)
+        {
+            displayed = !displayed;
+
+            if (displayed)
+            {
+                arrow.SetActive(true);
+                nextTime = Time.time + displayTime;
+            }
+            else
+            {
+                nextTime = Time.time + interval;
+                arrow.SetActive(false);
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ThirdPersonController_LITE")
+        {
+            // platform fall.
+            targetplatform
+                .GetComponent<fallingscript>()
+                .falling = true;
+                Renderer renderer = GetComponent<Renderer>();
+                spawner.SpawnBalls(renderer.material.color);
+        }
+    }
+}
